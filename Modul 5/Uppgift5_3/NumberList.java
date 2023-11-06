e10ef1a4d17e70423d0306f994f3e7590e125d64
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2023-11-06 14:46:26 +0000

    La till max, min, avg och rättade till syntaxfel

--- Modul 5/Uppgift5_3/NumberList.java
+++ Modul 5/Uppgift5_3/NumberList.java
@@ -10,25 +10,38 @@ public class NumberList
 {
     public static void main (String[] args)
     {
-      
+    int[] numbers = new int[args.length];
+
     //Konverterar argument(en) till heltal och lagrar dem i arrayen "numbers"
       for (int i = 0; i < args.lenth; i++) {
         numbers[i] = Integer.parseInt(args[i]);
 
     //Skriver ut den osorterade listan
-    System.out.print("Original: ")
+    System.out.print("Original: ");
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + ' ');
     System.out.println();
             
     //Sorterar listan i sorterad ordning och skriver ut den
-    java.util.Arrays.sort(numbers)
+    java.util.Arrays.sort(numbers);
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + ' ');
         }
     System.out.println();
 
+    //Hittar minsta och största värdet i arrayen
+    int min = numbers[0];
+    int max = numbers[numbers.length - 1];
+    System.out.println("Min: " + min);
+    System.out.println("Max: " + max);
 
+    //Beräknar genomsnittet
+    int sum = 0;
+    for (int i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    double average = (double) sum / numbers.length;
+    System.out.println("Avg: " + average);
 
         }
       }
