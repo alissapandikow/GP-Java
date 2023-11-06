f110437d4eedad455ce2b1eeaf19e3438af3c8df
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2023-11-06 13:33:35 +0000

    commit msg lol

---
+++ Modul 5/Uppgift5_2/Printer.java
@@ -0,0 +1,13 @@
+public class Printer {
+    public static void main(String[] args) {
+    int[] numbers = { 5, 0, -23 };
+    numbers[1] += 16;
+
+    int[] values = numbers;
+    values[0] = 10;
+    values[2] *= -2;
+
+    for (int i = 0; i < numbers.length; i++)
+        System.out.println (numbers[i]);
+    }
+}
