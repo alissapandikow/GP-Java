b87a891fbe8806e0ee5fea6cd2bf0a578cdc7707
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2023-11-09 13:59:18 +0000

    test for args

--- Modul 5/Uppgift5_5/NumberSquare.java
+++ Modul 5/Uppgift5_5/NumberSquare.java
@@ -6,12 +6,17 @@
     2023-11-09
 */
 
+import java.util.Math;
 import java.util.Arrays;
 
 public class NumberSquare {
     public static void main(String[] args) {
 
-        
+        int numberOfArgs = Math.sqrt(args.length()) //Vad räknar den med?
+        int numberOfRows =  Math.sqrt(args.length())
+
+        System.out.print(numberOfRows)
+
     }
 
 }
