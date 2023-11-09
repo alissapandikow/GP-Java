0d96882fd19011b874e73efadb4bb07f47409b04
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2023-11-09 13:17:13 +0000

    Added excersies 5

---
+++ Modul 5/Uppgift5_5/NumberSquare.java
@@ -0,0 +1,19 @@
+/*
+    Uppgift5_4
+    
+    
+    Alissa Pandikow, 23alpa03@hig.se
+    2023-11-09
+*/
+
+import java.util.Arrays;
+
+public class NumberSquare {
+    public static void main(String[] args) {
+
+        
+    }
+
+}
+
+    
